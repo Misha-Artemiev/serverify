a59871e4cad3de991fdbf355d661ba547405000a
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2024-12-03 13:21:42 +0000

    capitalise first character

--- Package.swift
+++ Package.swift
@@ -8,17 +8,17 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "serverify",
-            targets: ["serverify"]),
+            name: "Serverify",
+            targets: ["Serverify"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "serverify"),
+            name: "Serverify"),
         .testTarget(
-            name: "serverifyTests",
-            dependencies: ["serverify"]
+            name: "ServerifyTests",
+            dependencies: ["Serverify"]
         ),
     ]
 )
--- Tests/ServerifyTests/ServerifyTests.swift
+++ Tests/ServerifyTests/ServerifyTests.swift
@@ -1,5 +1,5 @@
 import Testing
-@testable import serverify
+@testable import Serverify
 
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
